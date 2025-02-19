1ec7485f9d00ae9717babd05f737d5c67ef26f30
Lumi1717 <ayhahlam623@gmail.com>
2025-02-20 01:32:20 +0400

    added json

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import mediapipe as mp
+import json
 
 image = cv2.imread("tom-hanks-niece.jpg")
 
@@ -14,14 +15,24 @@ result = face_mesh.process(rgb_image)
 
 height, width, _ = image.shape
 
-for facial_landmarks in result.multi_face_landmarks:
-    for i in range(0, 468):
-        pt1 = facial_landmarks.landmark[i]
-        x = int(pt1.x * width)
-        y = int(pt1.y * height)
+landmarks = []
 
-        cv2.circle(image, (x, y), 5, (100, 100, 0), -1)
+if result.multi_face_landmarks:
+    for facial_landmarks in result.multi_face_landmarks:
+        face_landmarks = []
+        for i in range(0, 468):
+            pt1 = facial_landmarks.landmark[i]
+            x = int(pt1.x * width)
+            y = int(pt1.y * height)
+            face_landmarks.append({"x": x, "y": y})
+            cv2.circle(image, (x, y), 5, (100, 100, 0), -1)
+        landmarks.append(face_landmarks)
+
+# Save landmarks as JSON
+with open("landmarks.json", "w") as json_file:
+    json.dump(landmarks, json_file, indent=4)
 
 cv2.imshow("Image", image)
 cv2.imwrite("output_image.png", image) # Save the image
 cv2.waitKey(0)
+cv2.destroyAllWindows()
